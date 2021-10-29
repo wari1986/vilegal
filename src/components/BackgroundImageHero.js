d883ced82202663e87625a8055c1419259e0e747
wari1986 <nicolaycamacho@gmail.com>
2021-10-29 16:05:58 +0200

    Header sticker removed, margin bottom added to contact button added and images added

--- src/components/BackgroundImageHero.js
+++ src/components/BackgroundImageHero.js
@@ -27,28 +27,30 @@ const GbiBridged = () => {
   const bgImage = convertToBgImage(image);
 
   return (
-    <BackgroundImage tag="section" {...bgImage} preserveStackingContext>
-      <div>
-        <section className="pt-20 md:pt-40">
-          <div className="container mx-auto px-8 lg:flex">
-            <div className="text-center lg:text-left lg:w-1/2">
-              <h1 className="text-white text-4xl lg:text-5xl xl:text-6xl font-bold leading-none">
-                Asesoramiento Legal Personalizado
-              </h1>
-              <p className="text-white text-xl lg:text-2xl mt-6 font-light">
-                Nuestro compromiso <br /> Satisfacción, Tranquilidad y Confianza
-              </p>
-              <p className="text-xl lg:text-2xl mt-6 font-light"></p>
-              <p className="mt-8 md:mt-12">
-                <Button size="lg">Contáctanos</Button>
-              </p>
-              {/* <p className="mt-4 text-gray-600">Nuestro personal estará feliz de atenderlo</p> */}
+      <BackgroundImage tag="section" {...bgImage} preserveStackingContext>
+        <div>
+          <section className="pt-20 md:pt-40">
+            <div className="container mx-auto px-8 lg:flex">
+              <div className="text-center lg:text-left lg:w-1/2">
+                <h1 className="text-white text-4xl lg:text-5xl xl:text-6xl font-bold leading-none">
+                  Asesoramiento Legal Personalizado
+                </h1>
+                <p className="text-white text-xl lg:text-2xl mt-6 font-light">
+                  Nuestro compromiso <br /> Satisfacción, Tranquilidad y Confianza
+                </p>
+                {/* <p className="text-xl lg:text-2xl mt-6 font-light"></p> */}
+                <p className="mt-8 md:mt-12">
+                  <Button className="m-16" size="lg">
+                    Contáctanos
+                  </Button>
+                </p>
+                {/* <p className="mt-4 text-gray-600">Nuestro personal estará feliz de atenderlo</p> */}
+              </div>
+              <div className="lg:w-1/2">{/* <HeroImage /> */}</div>
             </div>
-            <div className="lg:w-1/2">{/* <HeroImage /> */}</div>
-          </div>
-        </section>
-      </div>
-    </BackgroundImage>
+          </section>
+        </div>
+      </BackgroundImage>
   );
 };
 
