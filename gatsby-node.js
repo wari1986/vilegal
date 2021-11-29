3e67678bd16a90413c0f8ce24a8bee6cd10ce3b1
Nicolay Camacho <nicolaycamacho@gmail.com>
2021-11-30 00:22:25 +0100

    blog fixed

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,36 +1,36 @@
-const path = require(`path`);
+// const path = require(`path`);
 
-exports.createPages = async ({ actions, graphql, reporter }) => {
-  const { createPage } = actions;
+// exports.createPages = async ({ actions, graphql, reporter }) => {
+//   const { createPage } = actions;
 
-  const articleTemplate = path.resolve(`src/templates/articleTemplate.js`);
+//   const articleTemplate = path.resolve(`src/templates/articleTemplate.js`);
 
-  const result = await graphql(`
-    {
-      allMarkdownRemark {
-        edges {
-          node {
-            frontmatter {
-              path
-              type
-            }
-          }
-        }
-      }
-    }
-  `);
+//   const result = await graphql(`
+//     {
+//       allMarkdownRemark {
+//         edges {
+//           node {
+//             frontmatter {
+//               path
+//               type
+//             }
+//           }
+//         }
+//       }
+//     }
+//   `);
 
-  if (result.errors) {
-    reporter.panicOnBuild(`Error while running GraphQL query.`);
-    return;
-  }
+//   if (result.errors) {
+//     reporter.panicOnBuild(`Error while running GraphQL query.`);
+//     return;
+//   }
 
-  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+//   result.data.allMarkdownRemark.edges.forEach(({ node }) => {
 
-    createPage({
-      path: node.frontmatter.path,
-      component: articleTemplate,
-      context: {},
-    });
-  });
-};
+//     createPage({
+//       path: node.frontmatter.path,
+//       component: articleTemplate,
+//       context: {},
+//     });
+//   });
+// };
