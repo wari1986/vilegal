660786b409e24ad589365b10f5d5f5b0bb3ddc14
Nicolay Camacho <nicolaycamacho@gmail.com>
2021-11-30 01:23:54 +0100

    FooterBottom added to blog

--- src/components/layout/LayoutBlog.js
+++ src/components/layout/LayoutBlog.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Footer from './Footer';
 import Header from './HeaderBlog';
+import FooterBottom from './FooterBottom'
 
 const LayoutBlog = ({ children }) => {
   return (
@@ -8,6 +9,7 @@ const LayoutBlog = ({ children }) => {
       <Header />
         <main className="text-gray-900">{children}</main>
       <Footer />
+      <FooterBottom />
     </>
   );
 };
